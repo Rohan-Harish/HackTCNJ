0219e71e08a3c64bf1c9834fdd15759777b73df8
Rohan Harish <rohan.harish@gmail.com>
2020-02-29 20:04:08 -0500

    ServerCode

---
+++ HackTCNJ_2020/Server/server.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+
+app = Flask(__name__)
+
+names = ['rh388', 'vrp53', 'ps937', 'sn637']
+passwords = ['rohanpassword', 'varunpassword', 'prathishapassword', 'sahanapassword']
+
+@app.route('/login')
+def starter():
+    return render_template('index.html')
+
+
+@app.route('/authenticate', methods=["POST", "GET"])
+def authenticate():
+    if request.method == "POST":
+        if passwords[names.find(request.form['name'])] == request.form['password']:
+            return "Authenticated"
+        else:
+            return "error"
+    else:
+        if passwords[names.find(request.form['name'])] == request.form['password']:
+            return "Worked"
+        else:
+            return "error"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port = 6969)
